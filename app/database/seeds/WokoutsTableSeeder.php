ea10c6c2d030bd0aac70c3e1c83c1d9b8c07fb70
Zack Vera <qphys50@gmail.com>
2014-02-02 21:02:45 -0700

    Seeding

--- app/database/seeds/WokoutsTableSeeder.php
+++ app/database/seeds/WokoutsTableSeeder.php
@@ -10,17 +10,17 @@ class WorkoutsTableSeeder extends Seeder {
 
 		$faker = Faker\Factory::create();
 		$users = User::all();
-
 		foreach($users as $user) {
-			for ($i = 0; $i < 10; $i++) {
+			$first = Carbon::now()->subMonths(3);
+			for ($i = 0; $i < 30; $i++) {
 				$workout = Workout::create(
 					array(
 						"user_id" => $user->id,
 						"comment" => $faker->sentence(25),
-						"created_at" => Carbon::now(),
-						"updated_at" => Carbon::now()
+						"date" => $first,
 					)
 				);
+				$first = $first->addDays(3);
 			}
 		}
 
